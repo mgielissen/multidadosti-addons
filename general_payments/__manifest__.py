e61eeae0b4f559734b98b46b1c9ba3ca92c19498
Aldo_Soares <soares_aldo@hotmail.com>
2018-06-13 18:11:09 -0300

    [REF] Remove view de customização de 'account.move'

--- general_payments/__manifest__.py
+++ general_payments/__manifest__.py
@@ -16,7 +16,6 @@
     'data': [
         'templates/assets.xml',
         'views/account_analytic.xml',
-        'views/account_move.xml',
         'views/account_payment.xml',
     ],
     'installable': True,
