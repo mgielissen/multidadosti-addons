e3020f6575f7b0d48d031f22743b0ed7ec2bbf31
Michell Stuttgart <michellstut@gmail.com>
2018-10-09 12:07:50 -0300

    [DEL] Remove 'account_payment_project'

--- account_payment_project/models/account_payment.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models
-
-
-class AccountPayment(models.Model):
-    _inherit = 'account.payment'
-
-    project_id = fields.Many2one(string='Project',
-                                 comodel_name='project.project')
