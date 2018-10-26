afe356bf1418247765a8c6f1824af60aa858d3c2
Aldo Soares <soares_aldo@hotmail.com>
2018-10-26 16:41:08 -0300

    [NEW] Inclui módulo base de visualização de emails

---
+++ mail_access_emails/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': "Access Emails",
+    'summary': """
+        Access To Emails.""",
+    'description': """
+        This Base module includes access emails.
+    """,
+    'author': "MultidadosTI",
+    'website': "www.multidadosti.com.br",
+    'category': 'mail',
+    'version': '11.0.1.0.0',
+    'depends': ['mail'],
+    'data': [
+        'views/access_emails.xml',
+    ],
+}
